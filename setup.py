a02c541823d7e3d76cc001b18bc77002193e716e
alexphi <alexphi@umich.edu>
2020-07-13 21:47:36 -0700

    updated pypi name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='janus',
+setup(name='janus-highvar',
       version='0.0.1',
       description='Dungeons and Dragons High Variance Die Roller',
       author='Alex Philpott',
